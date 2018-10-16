d548159e8d408a0cc3eac4bffcb0251aa9943dd6
kokmok <jona303@gmail.com>
2018-10-16 11:48:36 +0200

    WIP twig lesson

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -10,43 +10,26 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
+    private function getAdverts(){
+      return [
+          ['id'=>1,"title"=>"title 1","author"=>"Gauthier","date"=>new \DateTime(),"content"=>"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."],
+          ['id'=>2,"title"=>"title 2","author"=>"Fanny","date"=>new \DateTime("1/4/1999"),"content"=>"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."],
+          ['id'=>3,"title"=>"title 3","author"=>"Anthony","date"=>new \DateTime("1/4/1998"),"content"=>"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."],
+      ];  
+    } 
     
-    public function indexAction($page, Request $request){
+    public function indexAction($page, Request $request) {
         
-        dump($request->query->get('tag')); //variable $_GET['tag']
-        dump($request->request->get('tag'));//variable $_POST['tag']
-        dump($request);
-        if ($request->isMethod(Request::METHOD_POST)){
-            dump("methode post");
-        }
-        elseif ($request->isMethod(Request::METHOD_GET)){
-            dump("methode get");
-        }
         
-        if ($request->isXmlHttpRequest()){
-            dump("ajax request");
-        }
-        dump($page);
-        
-        return new Response("Vous voyez la page ".$page."</body>");
+        return $this->render("@OCPlatform/Default/index.html.twig",["listAdverts"=>$this->getAdverts()]);
     }
+    
+    
     
     public function viewAction($id){
         
-        //Réponse simple
-        /* 
-        $response = new Response();
         
-        $response->setStatusCode(Response::HTTP_NOT_FOUND);
-        $response->setContent("ekufhgaqfs");
-        return $response;
-        */
-//        
-//        $response = new Response(); //Methode sans raccourci
-//        $response->setContent($this->get('twig')->render("@OCPlatform/Default/index.html.twig"));
-//        return $response;
-        //Raccourci de controller
-        return $this->render("@OCPlatform/Default/index.html.twig",['id'=>$id]);
+        return $this->render("@OCPlatform/Default/view.html.twig",['advert'=>$this->getAdverts()[$id-1]]);
 //        
 //        return $this->get('twig')->render("@OCPlatform/Default/index.html.twig");
 //        
@@ -72,6 +55,10 @@ class AdvertController extends Controller
     
     public function deleteAction($id){
 
+    }
+    
+    public function menuAction(){
+        return $this->render("@OCPlatform/Default/menu.html.twig");
     }
     
 
